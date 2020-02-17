5042eff470611f3c66ef7656d3823270ec069662
MaxKuzaj13 <maksymilian.rogowski@gmail.com>
2020-02-17 20:55:39 +0100

    redy to test code

--- guessing_game.py
+++ guessing_game.py
@@ -1,49 +1,56 @@
-import math
 import random
-a = []
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-for i in range(10):
-    g = int(input("Enter an integer from 1 to 99: "))
-    while a[i] != g:
-        if g < a[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 99: "))
-        elif g > a[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 99: "))
-        else:
-            break
-    print("you guessed it!")
 
-b = []
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-for i in range(10):
-    g = int(input("Enter an integer from 1 to 49: "))
-    while b[i] != g:
-        if g < b[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 49: "))
-        elif g > b[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 49: "))
-        else:
-            break
-    print("you guessed it!")
+RANGE_OF_TRIES = range(0, 10)
+MIN_RANGE_TO_GUESS = 1
+guess = 0
+
+
+def draw_numbers_to_guess(RANGE_OF_TRIES, MIN_RANGE_TO_GUESS, max_range_to_guess):
+    a_list_of_numbers_to_guess = []
+    for round_number in RANGE_OF_TRIES:
+        a_list_of_numbers_to_guess.append(random.randint(MIN_RANGE_TO_GUESS, max_range_to_guess))
+
+    return a_list_of_numbers_to_guess
+
+
+def ask_for_guess(MAX_RANGE_TO_GUESS):
+    exit_value = False
+    while exit_value != True:
+        try:
+            guess = int(input(f"Enter an integer from {MIN_RANGE_TO_GUESS} to {MAX_RANGE_TO_GUESS}: "))
+            exit_value = True
+        except ValueError:
+            print('Pleas enter correct integer')
+
+    return guess
+
+
+def game_initiation(RANGE_OF_TRIES, a_list_of_numbers_to_guess, guess, max_range_to_guess):
+    for number_shots in RANGE_OF_TRIES:
+        while a_list_of_numbers_to_guess[number_shots] != guess:
+            guess = ask_for_guess(max_range_to_guess)
+            if guess < a_list_of_numbers_to_guess[number_shots]:
+                print("guess is low")
+            elif guess > a_list_of_numbers_to_guess[number_shots]:
+                print("guess is high")
+            else:
+                break
+        print("you guessed it!")
+
+
+def main():
+    max_range_to_guess = 99
+    a_list_of_numbers_to_guess = draw_numbers_to_guess(RANGE_OF_TRIES, MIN_RANGE_TO_GUESS, max_range_to_guess)
+    print(a_list_of_numbers_to_guess)
+    game_initiation(RANGE_OF_TRIES, a_list_of_numbers_to_guess, guess, max_range_to_guess)
+    # changing max of range to second game
+    max_range_to_guess = 49
+    a_list_of_numbers_to_guess = draw_numbers_to_guess(RANGE_OF_TRIES, MIN_RANGE_TO_GUESS, max_range_to_guess)
+    print(a_list_of_numbers_to_guess)
+    game_initiation(RANGE_OF_TRIES, a_list_of_numbers_to_guess, guess, max_range_to_guess)
+
+
+if __name__ == '__main__':
+    main()
+
+
